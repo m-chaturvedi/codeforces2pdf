81b8a9b4f74e13f86e3f954c6b3c6090dc608f0c
Mmanu Chaturvedi <mmanu.chaturvedi@scania.com>
2024-02-17 00:05:16 -0500

    Update download script

--- download.sh
+++ download.sh
@@ -2,25 +2,30 @@
 set -euo pipefail
 
 declare -a A=(
-	"1928/C"
-	"1928/D"
-	"1931/F"
-	"1928/C"
-	"1924/A"
-	"1922/E"
-	"1921/E"
-	"1920/C"
-	"1917/C"
-	"1916/D"
-	"1915/G"
-	"1915/F"
-	"1912/K"
-	"1910/D"
+	"1928_C"
+	"1928_D"
+	"1931_F"
+	"1928_C"
+	"1924_A"
+	"1922_E"
+	"1921_E"
+	"1920_C"
+	"1917_C"
+	"1916_D"
+	"1915_G"
+	"1915_F"
+	"1912_K"
+	"1910_D"
 )
 
 [[ -d "$1" ]] || ( echo "Directory does not exist: $1" && exit 1 )
 
 for a in "${A[@]}"; do
-	wkhtmltopdf --quiet "https://codeforces.com/problemset/problem/$a" \
-		"$1/$( echo $a | tr '/' '_').pdf"
+	wkhtmltopdf --quiet "https://codeforces.com/problemset/problem/$( echo $a | tr '_' '/')" \
+		"$1/$a"
 done
+
+CURRENT_DIR=$PWD
+pushd "$1"
+	pdftk ${A[@]} cat output ${CURRENT_DIR}/concatenated_1.pdf
+popd
