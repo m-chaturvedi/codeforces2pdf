de80492b1e38018e4706244e7ca2d2964036b675
Mmanu Chaturvedi <mmanu.chaturvedi@scania.com>
2024-02-17 00:06:05 -0500

    Update download script

--- download.sh
+++ download.sh
@@ -18,14 +18,14 @@ declare -a A=(
 	"1910_D"
 )
 
-[[ -d "$1" ]] || ( echo "Directory does not exist: $1" && exit 1 )
+[[ -d "$1" ]] || (echo "Directory does not exist: $1" && exit 1)
 
 for a in "${A[@]}"; do
-	wkhtmltopdf --quiet "https://codeforces.com/problemset/problem/$( echo $a | tr '_' '/')" \
+	wkhtmltopdf --quiet "https://codeforces.com/problemset/problem/$(echo $a | tr '_' '/')" \
 		"$1/$a"
 done
 
 CURRENT_DIR=$PWD
 pushd "$1"
-	pdftk ${A[@]} cat output ${CURRENT_DIR}/concatenated_1.pdf
+pdftk ${A[@]} cat output ${CURRENT_DIR}/concatenated_1.pdf
 popd
